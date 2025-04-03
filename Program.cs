6dd3518e989b47dccbc1b9dcd407da194587baba
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-04-03 03:50:41 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using static System.Net.Mime.MediaTypeNames;
+using System.Configuration;
+//this code will initialize the project
+namespace poe_part1_prog
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //setting the color to br white 
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("=======================================================================================");
+
+            // changing the color for AI bot
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("  Hi welcome to the cybersecurity awwreness bot fell free to ask e questions! ");
+            //changing the color 
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("=======================================================================================");
+            //Create filter and split class for user questions
+            
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+    }
+}
+
+
